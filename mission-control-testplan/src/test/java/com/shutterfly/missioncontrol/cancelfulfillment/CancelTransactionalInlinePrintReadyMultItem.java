36b58dab04611e055b177b2f56a5dcc22bfe9c3c
Shweta <skamthane@shutterfly.com>
2018-01-04 10:43:42 +0530

    SMC-5665 Changes for cancel of multiitem

--- mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/cancelfulfillment/CancelTransactionalInlinePrintReadyMultItem.java
+++ mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/cancelfulfillment/CancelTransactionalInlinePrintReadyMultItem.java
@@ -68,7 +68,8 @@ public class CancelTransactionalInlinePrintReadyMultItem extends ConfigLoader {
   @Test(groups = "database", dependsOnGroups = {"Test_CTIPRMI_XML"})
   private void validateRecordsInDatabase() throws Exception {
     DatabaseValidationUtil databaseValidationUtil = new DatabaseValidationUtil();
-    databaseValidationUtil.validateRecordsAvailabilityAndStatusCheck(record, "AcceptedBySupplier",
-        AppConstants.CANCEL);
+    databaseValidationUtil
+        .validateRecordsAvailabilityAndStatusCheck(record, "PutToRequestGeneratorTopic",
+            AppConstants.CANCEL);
   }
 }
