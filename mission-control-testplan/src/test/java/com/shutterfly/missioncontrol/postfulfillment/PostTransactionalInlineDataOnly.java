3298e74d5caa387d767685b39b6d48002c5fee70
Shweta <skamthane@shutterfly.com>
2018-01-11 17:54:15 +0530

    SMC-5778 test

--- mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/postfulfillment/PostTransactionalInlineDataOnly.java
+++ mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/postfulfillment/PostTransactionalInlineDataOnly.java
@@ -6,20 +6,20 @@ package com.shutterfly.missioncontrol.postfulfillment;
 import static org.hamcrest.Matchers.equalTo;
 import static org.testng.Assert.assertEquals;
 
-import com.shutterfly.missioncontrol.common.AppConstants;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
-import org.testng.annotations.Test;
-
 import com.google.common.io.Resources;
+import com.shutterfly.missioncontrol.common.AppConstants;
 import com.shutterfly.missioncontrol.common.DatabaseValidationUtil;
 import com.shutterfly.missioncontrol.config.ConfigLoader;
 import com.shutterfly.missioncontrol.config.CsvReaderWriter;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+import org.testng.annotations.Test;
 
 /**
  * @author dgupta
@@ -32,6 +32,7 @@ public class PostTransactionalInlineDataOnly extends ConfigLoader {
   private String uri = "";
   private String payload = "";
   private String record = "";
+  DatabaseValidationUtil databaseValidationUtil = new DatabaseValidationUtil();
 
   private String getProperties() {
     basicConfigNonWeb();
@@ -68,9 +69,16 @@ public class PostTransactionalInlineDataOnly extends ConfigLoader {
 
   @Test(groups = "Post_TIDO_DB", dependsOnGroups = {"Post_TIDO_Response"})
   private void validateRecordsInDatabase() throws Exception {
-    DatabaseValidationUtil databaseValidationUtil = new DatabaseValidationUtil();
     databaseValidationUtil
         .validateRecordsAvailabilityAndStatusCheck(record, AppConstants.ACCEPTED_BY_REQUESTOR,
             AppConstants.POST_STATUS);
+  }
+
+  @Test(groups = "Post_TIDO_DB_RequestHistory", dependsOnGroups = {"Post_TIDO_Response"})
+  private void validateRequestHistoryInDatabase() throws Exception {
+    Document fulfillmentTrackingRecordDoc = databaseValidationUtil.getTrackingRecord(record);
+    List<Document> eventHistory = (ArrayList<Document>) fulfillmentTrackingRecordDoc
+        .get("eventHistory");
+    assertEquals(eventHistory.get(1).get("eventType").toString(), "Received");
   }
 }
--- mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/processfulfillment/TransactionalInlineDataOnly.java
+++ mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/processfulfillment/TransactionalInlineDataOnly.java
@@ -3,26 +3,23 @@
  */
 package com.shutterfly.missioncontrol.processfulfillment;
 
-import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
-
-import com.shutterfly.missioncontrol.common.AppConstants;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.UUID;
-
-import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.equalTo;
 
 import com.google.common.io.Resources;
+import com.shutterfly.missioncontrol.common.AppConstants;
 import com.shutterfly.missioncontrol.common.DatabaseValidationUtil;
 import com.shutterfly.missioncontrol.config.ConfigLoader;
 import com.shutterfly.missioncontrol.config.CsvReaderWriter;
-
 import io.restassured.RestAssured;
 import io.restassured.config.EncoderConfig;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import org.testng.annotations.Test;
 
 /**
  * @author dgupta
