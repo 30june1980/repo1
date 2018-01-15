fd1bfbd5eb5022296e20096cbf5d4e78383e35a6
mohammad ruksad <v-rsiddiqui@shutterfly.com>
2018-01-15 18:01:52 +0530

    SMC-5838 process archive validation

--- mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/processarchive/ProcessArchiveTransactionInlineDataOnly.java
+++ mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/processarchive/ProcessArchiveTransactionInlineDataOnly.java
@@ -21,6 +21,8 @@ import io.restassured.response.Response;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 /**
@@ -31,6 +33,8 @@ public class ProcessArchiveTransactionInlineDataOnly extends ConfigLoader {
   private String uri = "";
   private String payload = "";
   private String record = "";
+
+  Logger logger= LoggerFactory.getLogger(ProcessArchiveTransactionInlineDataOnly.class);
 
   private String getProperties() {
     basicConfigNonWeb();
@@ -78,20 +82,47 @@ public class ProcessArchiveTransactionInlineDataOnly extends ConfigLoader {
     basicConfigNonWeb();
     EncoderConfig encoderconfig = new EncoderConfig();
     String payload = Utils.replaceExactMatch(this.buildPayload(),"Archive","Anything");
-    EcgFileSafeUtil.putFileAtSourceLocation(EcgFileSafeUtil.buildInboundFilePath(payload), record,
-        AppConstants.BULK_FILE);
     Response response = given()
         .config(RestAssured.config()
             .encoderConfig(
                 encoderconfig.appendDefaultContentCharsetToContentTypeIfUndefined(false)))
         .header("saml", config.getProperty("SamlValue")).contentType(ContentType.XML).log().all()
-        .body(this.buildPayload()).when().post(this.getProperties());
+        .body(payload).when().post(this.getProperties());
     assertEquals(response.getStatusCode(), 200, "Assertion for Response code!");
     response.then().body(
-        "acknowledgeMsg.acknowledge.validationResults.transactionLevelAck.transaction.transactionLevelErrors.transactionError.errorCode",
+        "acknowledgeMsg.acknowledge.validationResults.transactionLevelAck.transaction.transactionLevelErrors.transactionError.errorCode.code",
         equalTo("18011"));
   }
 
+  private String buildPayloadForInvalidArchive() throws IOException {
+    URL file = Resources
+        .getResource("XMLPayload/Validation/ProcessArchiveTransactionalInlineDataOnlyInvalidArchiveDetails.xml");
+    payload = Resources.toString(file, StandardCharsets.UTF_8);
+    return payload = payload.replaceAll("REQUEST_101", record)
+        .replaceAll("bulkfile_all_valid.xml", (record + ".xml"));
+
+  }
+
+  @Test()
+  private void getResponseForMissingRequestDetails() throws IOException {
+    basicConfigNonWeb();
+    EncoderConfig encoderconfig = new EncoderConfig();
+    Response response = null;
+    try {
+      response = given()
+          .config(RestAssured.config()
+              .encoderConfig(
+                  encoderconfig.appendDefaultContentCharsetToContentTypeIfUndefined(false)))
+          .header("saml", config.getProperty("SamlValue")).contentType(ContentType.XML).log().all()
+          .body(this.buildPayloadForInvalidArchive()).when().post(this.getProperties());
+    } catch (Exception e) {
+      logger.info("Could not make rest assured call {}",e.getMessage());
+    }
+    assertEquals(response.getStatusCode(), 200, "Assertion for Response code!");
+    response.then().body(
+        "acknowledgeMsg.acknowledge.validationResults.transactionLevelAck.transaction.transactionLevelErrors.transactionError.errorCode.code",
+        equalTo("18073"));
+  }
 
   @Test(groups = "Archive_TIDO_DB", dependsOnGroups = {"Archive_TIDO_Response"})
   private void validateRecordsInDatabase() throws Exception {
