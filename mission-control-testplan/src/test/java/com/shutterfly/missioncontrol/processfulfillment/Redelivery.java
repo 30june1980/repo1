3d9ecc35294baacfe7ebf471b0d0b1e4a5fed1cf
Shweta <skamthane@shutterfly.com>
2018-01-25 17:29:18 +0530

    SMC-5910: Redelivery

---
+++ mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/processfulfillment/Redelivery.java
@@ -0,0 +1,76 @@
+package com.shutterfly.missioncontrol.processfulfillment;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.assertEquals;
+
+import com.google.common.io.Resources;
+import com.shutterfly.missioncontrol.common.AppConstants;
+import com.shutterfly.missioncontrol.common.EcgFileSafeUtil;
+import com.shutterfly.missioncontrol.common.ValidationUtilConfig;
+import com.shutterfly.missioncontrol.config.ConfigLoader;
+import com.shutterfly.missioncontrol.config.CsvReaderWriter;
+import io.restassured.RestAssured;
+import io.restassured.config.EncoderConfig;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import org.testng.annotations.Test;
+
+
+public class Redelivery extends ConfigLoader {
+
+  private String uri = "";
+
+  UUID uuid = UUID.randomUUID();
+  String record = "Test_qa_" + uuid.toString();
+  CsvReaderWriter cwr = new CsvReaderWriter();
+
+  private String getProperties() {
+    basicConfigNonWeb();
+    uri = config.getProperty("BaseUrl") + config.getProperty("UrlExtensionProcessFulfillment");
+    return uri;
+  }
+
+  private String buildPayload() throws IOException {
+    URL file = Resources
+        .getResource("XMLPayload/ProcessFulfillment/TransactionalExternalPrintReady_Redelivery.xml");
+    String payload = Resources.toString(file, StandardCharsets.UTF_8);
+    return payload.replaceAll("REQUEST_101", record).replaceAll("bulkfile_all_valid.xml",
+        (record + ".xml"));
+  }
+
+
+  @Test(groups = "Process_Redelivery_Response")
+  private void getResponse() throws IOException {
+    basicConfigNonWeb();
+    String payload = this.buildPayload();
+
+    EcgFileSafeUtil.putFileAtSourceLocation(EcgFileSafeUtil.buildInboundFilePath(payload),
+        record, AppConstants.BULK_FILE);
+
+    EncoderConfig encoderconfig = new EncoderConfig();
+    Response response = given()
+        .config(RestAssured.config()
+            .encoderConfig(
+                encoderconfig.appendDefaultContentCharsetToContentTypeIfUndefined(false)))
+        .header("saml", config.getProperty("SamlValue")).contentType(ContentType.XML).log().all()
+        .body(this.buildPayload()).when().post(this.getProperties());
+    assertEquals(response.getStatusCode(), 200, "Assertion for Response code!");
+    response.then().body(
+        "acknowledgeMsg.acknowledge.validationResults.transactionLevelAck.transaction.transactionStatus",
+        equalTo("Accepted"));
+    cwr.writeToCsv("Redelivery", record);
+  }
+
+  @Test(groups = "Process_Redelivery_DB", dependsOnGroups = {"Process_Redelivery_Response"})
+  private void validateRecordsInDatabase() throws Exception {
+    ValidationUtilConfig.getInstances()
+        .validateRecordsAvailabilityAndStatusCheck(record, AppConstants.ACCEPTED_BY_SUPPLIER,
+            AppConstants.PROCESS);
+  }
+
+}
