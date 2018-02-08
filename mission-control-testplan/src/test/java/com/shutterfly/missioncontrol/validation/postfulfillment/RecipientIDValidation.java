b5f2dafffb0f3ad3da53e8e6ac50b441ef09de87
Shweta <skamthane@shutterfly.com>
2018-02-08 15:40:37 +0530

    SMC-6105 Adding tests

---
+++ mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/validation/postfulfillment/RecipientIDValidation.java
@@ -0,0 +1,65 @@
+package com.shutterfly.missioncontrol.validation.postfulfillment;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.assertEquals;
+
+import com.google.common.io.Resources;
+import com.shutterfly.missioncontrol.config.ConfigLoader;
+import com.shutterfly.missioncontrol.util.RequestUtil;
+import io.restassured.RestAssured;
+import io.restassured.config.EncoderConfig;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import org.testng.annotations.Test;
+
+public class RecipientIDValidation extends ConfigLoader {
+
+  private String uri = "";
+  UUID uuid = UUID.randomUUID();
+  String record = "Test_qa_" + uuid.toString();
+
+  private String getProperties() {
+    basicConfigNonWeb();
+    uri = config.getProperty("BaseUrl") + config.getProperty("UrlExtensionPostFulfillment");
+    return uri;
+  }
+
+  private String buildPayload() throws IOException {
+    URL file = Resources
+        .getResource("XMLPayload/PostFulfillment/PostTransactionalInlineDataOnly.xml");
+    String payload = Resources.toString(file, StandardCharsets.UTF_8);
+    return payload.replaceAll("REQUEST_101", record).replaceAll("111223333", "");
+  }
+
+  @Test
+  private void getResponse() throws Exception {
+    RequestUtil.sendProcess(record);
+    basicConfigNonWeb();
+    EncoderConfig encoderconfig = new EncoderConfig();
+    Response response = given()
+        .config(RestAssured.config()
+            .encoderConfig(
+                encoderconfig.appendDefaultContentCharsetToContentTypeIfUndefined(false)))
+        .header("saml", config.getProperty("SamlValue")).contentType(ContentType.XML).log().all()
+        .body(this.buildPayload()).when().post(this.getProperties());
+
+    assertEquals(response.getStatusCode(), 200, "Assertion for Response code!");
+    System.out.println(response.getBody().asString());
+    response.then().body(
+        "acknowledgeMsg.acknowledge.validationResults.transactionLevelAck.transaction.transactionStatus",
+        equalTo("Rejected"));
+    response.then().body(
+        "acknowledgeMsg.acknowledge.validationResults.transactionLevelAck.transaction.transactionLevelErrors.transactionError.errorCode.code",
+        equalTo("18412"));
+
+    response.then().body(
+        "acknowledgeMsg.acknowledge.validationResults.transactionLevelAck.transaction.transactionLevelErrors.transactionError.errorCode.desc",
+        equalTo("Recipient ID is missing."));
+
+  }
+}
