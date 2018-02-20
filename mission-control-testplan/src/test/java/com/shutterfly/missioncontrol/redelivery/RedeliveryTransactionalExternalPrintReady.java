4940dd1b691fe8c4c933d7f031f8aea2de7c1e08
ldeshpande <ldeshpande@shutterfly.com>
2018-02-20 12:42:55 +0530

    SMC-6118 Redelivery

--- mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/redelivery/RedeliveryTransactionalExternalPrintReady.java
+++ mission-control-testplan/src/test/java/com/shutterfly/missioncontrol/redelivery/RedeliveryTransactionalExternalPrintReady.java
@@ -3,14 +3,24 @@ package com.shutterfly.missioncontrol.redelivery;
 import com.google.common.io.Resources;
 import com.shutterfly.missioncontrol.accesstoken.AccessToken;
 import com.shutterfly.missioncontrol.config.ConfigLoader;
+import com.shutterfly.missioncontrol.config.CsvReaderWriter;
+import com.shutterfly.missioncontrolportal.Utils.XmlUtils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
 
@@ -32,12 +42,37 @@ public class RedeliveryTransactionalExternalPrintReady extends ConfigLoader {
     }
 
     private String buildPayload() throws IOException {
+        Map<String, String> postMap = XmlUtils.readXml(Resources.getResource("XMLPayload/Redelivery/PostTransactionalExternalPrintReady.xml").getPath());
+        Map<String, String> processMap = XmlUtils.readXml(Resources.getResource("XMLPayload/Redelivery/TransactionalExternalPrintReady.xml").getPath());
+
         URL file = Resources.getResource("JSONPayload/FindFulfillmentHistoryRedelivery.json");
-        return Resources.toString(file, StandardCharsets.UTF_8);
+        String payload = Resources.toString(file, StandardCharsets.UTF_8);
+        JSONObject redeliveryObject = new JSONObject(payload);
+        redeliveryObject.put("fulfillmentType", postMap.get("sch:fulfillmentType"));
+        redeliveryObject.put("requestID", "");
+        redeliveryObject.put("searchSourceID", postMap.get("sch:sourceID"));
+        redeliveryObject.put("requestorRefNo", postMap.get("sch:requestorRefNo"));
+        redeliveryObject.put("supplierRefNo", postMap.get("sch:supplierRefNo"));
+        redeliveryObject.put("recipientID", postMap.get("sch:recipientId"));
+        redeliveryObject.put("eventDispatchedDate", formatDate(postMap.get("sch:dispatchedDate"), 1));
+        redeliveryObject.put("deliveryMethodCd", postMap.get("sch:deliveryMethodCd"));
+        redeliveryObject.put("idQualifier", processMap.get("ns2:idQualifier"));
+        redeliveryObject.put("recipientType", processMap.get("ns2:recipientType"));
+        redeliveryObject.put("rangeStartEventReceivedDate", formatDate(postMap.get("sch:receivedDate"), -20));
+        redeliveryObject.put("rangeEndEventReceivedDate", formatDate(postMap.get("sch:receivedDate"), 1));
+        return redeliveryObject.toString();
     }
 
-    @Test(dependsOnGroups = {"Process_EUTEPR_DB", "Post_EUTEPR_DB"})
-    public void redeliveryTest() throws IOException {
+    private String formatDate(@Nonnull String utcDate, int offset) {
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.parse(utcDate), ZoneOffset.UTC);
+        LocalDate localDate;
+        localDate = localDateTime.toLocalDate().plusDays(offset);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        return localDate.format(formatter);
+    }
+
+    @Test()
+    public void redeliveryTest() throws Exception {
         String payload = buildPayload();
         Response response = RestAssured.given().header("saml", config.getProperty("SamlValue")).log()
                 .all().contentType("application/json").accept("application/json")
